925c26754ef74693f68c214c6a05d0d408ca84c8
titanke <81938777+titanke@users.noreply.github.com>
2023-10-28 08:47:01 -0500

    estructura inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 //import { ApolloClientTest } from "./components/ApolloClientTest";
-
+import Chat  from "./components/pantalla_p";
 function App() {
-    return <>{/*<ApolloClientTest /> */}</>;
+    return <><Chat /> </>;
 }
 
 export default App;
